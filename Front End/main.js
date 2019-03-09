beec22cb350da7f915d94d6d664a83e93ed087f4
Gurvir Singh <38177703+gurvir14128@users.noreply.github.com>
2019-03-09 02:43:29 -0500

    Add files via upload

---
+++ Front End/main.js
@@ -0,0 +1,76 @@
+  var config = {
+    apiKey: "AIzaSyCU87yhweJP3j2Y4DvPMgPZT-0ouAflB80",
+    authDomain: "capstoneteam4-bb4ce.firebaseapp.com",
+    databaseURL: "https://capstoneteam4-bb4ce.firebaseio.com",
+    projectId: "capstoneteam4-bb4ce",
+    storageBucket: "capstoneteam4-bb4ce.appspot.com",
+    messagingSenderId: "110799787101"
+  };
+  firebase.initializeApp(config);
+
+var messageRef = firebase.database().ref('TravelCustomer');
+
+document.getElementById('registerForm').addEventListener('submit', submitForm);
+
+function submitForm(e) {
+	e.preventDefault();
+
+	var username = getInput('username');
+	var password = getInput('password');
+	var pass2 = getInput('pass2');
+	var firstName = getInput('firstName');
+	var lastName = getInput('lastName');
+	var email = getInput('email');
+	var phone = getInput('phone');
+	var street = getInput('street');
+	var city = getInput('city');
+	var state = getInput('state');
+	var zip = getInput('zip');
+
+	//Only thing code is missing is a way to check if the username is already taken. 
+	
+	if(password == pass2) {
+		saveMessage(username, password, firstName, lastName, email, phone, street, city, state, zip);
+		document.getElementById('registerForm').reset();
+	}
+	
+
+}
+
+function getInput(id) {
+	return document.getElementById(id).value;
+}
+
+function saveMessage(username, password, firstName, lastName, email, phone, street, city, state, zip) {
+	var newMessageRef = messageRef.push();
+	newMessageRef.set({
+		username:username,
+		password:password,
+		firstName:firstName,
+		lastName:lastName,
+		email:email,
+		phone:phone,
+		street:street,
+		city:city,
+		state:state,
+		zip:zip
+	});
+}
+
+function passMiss(){
+            alert("The passwords don't match");
+        }
+
+function successR(){
+            alert("Registered successfully");
+        }
+
+function match() {
+  if (getInput('password') == getInput('pass2')) {
+    document.getElementById('matching').style.color = 'green';
+    document.getElementById('matching').innerHTML = 'Passwords Match';
+  } else {
+    document.getElementById('matching').style.color = 'red';
+    document.getElementById('matching').innerHTML = 'Passwords do not match';
+  }
+}
